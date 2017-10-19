8c0cd48e750aa2958a293dc7546eab5a6c252cf9
jonseijo <jon.seijo@gmail.com>
2017-10-19 11:45:33 -0300

    Arregla la escala de los graficos de kfold

--- experimentos/grafKfold.py
+++ experimentos/grafKfold.py
@@ -2,6 +2,7 @@ from mpl_toolkits.mplot3d.axes3d import Axes3D
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import seaborn as sns
 
 
 Kkfold = []
@@ -36,7 +37,7 @@ with open('result.txt') as my_file:
         			if comas == 1:
         				inicio = i+1
         				while(line[inicio] != ','):
-        					
+
         					if line[inicio] != ' ':
         						escribir += line[inicio]
         					inicio+=1
@@ -44,7 +45,7 @@ with open('result.txt') as my_file:
         			elif comas == 2:
         				inicio = i+1
         				while(line[inicio] != ','):
-        					
+
         					if line[inicio] != ' ':
         						escribir += line[inicio]
         					inicio+=1
@@ -52,7 +53,7 @@ with open('result.txt') as my_file:
         			elif comas == 3:
         				inicio = i+1
         				while(line[inicio] != ','):
-        					
+
         					if line[inicio] != ' ':
         						escribir += line[inicio]
         					inicio+=1
@@ -60,7 +61,7 @@ with open('result.txt') as my_file:
         			elif comas == 4:
         				inicio = i+1
         				while(line[inicio] != ','):
-        					
+
         					if line[inicio] != ' ':
         						escribir += line[inicio]
         					inicio+=1
@@ -68,7 +69,7 @@ with open('result.txt') as my_file:
         			elif comas == 5:
         				inicio = i+1
         				while(line[inicio] != ','):
-        					
+
         					if line[inicio] != ' ':
         						escribir += line[inicio]
         					inicio+=1
@@ -76,7 +77,7 @@ with open('result.txt') as my_file:
         			elif comas == 7:
         				inicio = i+1
         				while(line[inicio] != ','):
-        					
+
         					if line[inicio] != ' ':
         						escribir += line[inicio]
         					inicio+=1
@@ -84,21 +85,24 @@ with open('result.txt') as my_file:
         			elif comas == 8:
         				inicio = i+1
         				while(line[inicio] != '\n'):
-        					
+
         					if line[inicio] != ' ':
         						escribir += line[inicio]
         					inicio+=1
         				tiempototalfold.append(float(escribir))
-    
+
 
 tiempototalfold_0 = []
-tiempototalknn_0 = []  
+tiempototalknn_0 = []
 pres_0 = []
 recall_0 = []
-f1_0 = []  
-eje_x = []  		
+f1_0 = []
+eje_x = []
+
+clase_elegida = 9
+
 for j in range(0, len(precision)):
-    if j%10 == 5: #cambiar acá para hacer un grafico para c/ clase
+    if j%10 == clase_elegida: #cambiar acá para hacer un grafico para c/ clase
         pres_0.append(precision[j])
         recall_0.append(recall[j])
         f1_0.append(f1[j])
@@ -112,12 +116,8 @@ for j in range(0, len(tiempototalfold_0)):
     tiempototalfold_0[j] /= 1000000000.0
     tiempototalknn_0[j] /= 1000000000.0
 
-
-
-print(precision)
-print(pres_0)
-
-
+# print(precision)
+# print(pres_0)
 
 #range = []
 #with open('range.txt') as my_file:
@@ -129,15 +129,27 @@ print(pres_0)
 #for k in range (1, 10):
     #eje_y.append(float(1/k))
 #eje_y = np.array[0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-plt.plot(eje_x, pres_0, '-o', label="precision")
 #plt.plot(eje_x, tiempototalfold_0, '-o', label="Tiempo total")
-plt.plot(eje_x, recall_0, '-o', label="recall")
-plt.plot(eje_x, f1_0, '-o',  label="f1")
-#plt.ylabel('Tiempo (seg)')
 
+# plt.plot(eje_x, pres_0, '-o', label="precision")
+# plt.plot(eje_x, recall_0, '-o', label="recall")
+# plt.plot(eje_x, f1_0, '-o',  label="f1")
+
+plt.plot(eje_x, pres_0, label="precision")
+plt.plot(eje_x, f1_0,  label="f1")
+plt.plot(eje_x, recall_0, label="recall")
+
+#plt.ylabel('Tiempo (seg)')
 plt.ylabel('Puntaje')
 plt.xlabel('k fold')
+plt.title('Clase ' + str(clase_elegida), fontsize=14)
+plt.ylim([0.8,1.1])
+
 #plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
-           ncol=2, mode="expand", borderaxespad=0.)
+
+plt.legend(["Precision", "F1", "Recall"])
+
+# plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           # ncol=2, mode="expand", borderaxespad=0.)
+
 plt.show()
