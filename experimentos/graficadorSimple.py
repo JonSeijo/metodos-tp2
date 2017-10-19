4e0cfc5d81441f67c9ff3a37cc0ad4d0fbaa2b4f
jonseijo <jon.seijo@gmail.com>
2017-10-19 02:11:02 -0300

    Agrega experimentacion en Kaggle - pca

--- experimentos/graficadorSimple.py
+++ experimentos/graficadorSimple.py
@@ -11,20 +11,21 @@ violeta = '#591463'
 
 colores = [azul, verde, rojo, amarillo, violeta]
 
-data = pd.read_csv('kkaggle.csv')
-data_kaggle = data.groupby('k')
+# data = pd.read_csv('kkaggle.csv')
+data = pd.read_csv('pcaKaggle.csv')
+data_kaggle = data.groupby('alpha')
 
 plt.clf()
 
 plot_grafo = (data_kaggle['score'].median()).plot(fontsize=13, figsize=(11,8), color=colores[0],
                 linestyle='--', marker='o')
 
-plot_grafo.set_title("Score en Kaggle - Sólo kNN\n42000 imágenes de training", fontsize = 15)
-plot_grafo.set_ylabel("Score", size = 14)
-plot_grafo.set_xlabel("k (kNN)", size = 14)
-plot_grafo.legend(["Score - Training 42000"], fontsize = 14)
+plot_grafo.set_title("Accuracy en Kaggle - PSA + kNN, k = 4, \n42000 imágenes de training", fontsize = 15)
+plot_grafo.set_ylabel("Accuracy", size = 14)
+plot_grafo.set_xlabel("Alpha", size = 14)
+plot_grafo.legend(["Accuracy PCA - 42000"], fontsize = 14)
 
 plt.ylim([0.9,1.04])
-plt.xlim([0,21])
+plt.xlim([25,38])
 
 plt.show()
