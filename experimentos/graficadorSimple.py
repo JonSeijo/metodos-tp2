f02983ed1505ed75d7ce25f1149a1a9105ce8ae3
jonseijo <jon.seijo@gmail.com>
2017-10-19 15:41:02 -0300

    Experimentacion tiempos pca vs knn

--- experimentos/graficadorSimple.py
+++ experimentos/graficadorSimple.py
@@ -12,20 +12,34 @@ violeta = '#591463'
 colores = [azul, verde, rojo, amarillo, violeta]
 
 # data = pd.read_csv('kkaggle.csv')
-data = pd.read_csv('pcaKaggle.csv')
-data_kaggle = data.groupby('alpha')
+# data = pd.read_csv('pcaKaggle.csv')
+# data_kaggle = data.groupby('alpha')
+
+data_knn = pd.read_csv('knnTiempos.csv')
+data_psa = pd.read_csv('psaTiempos.csv')
+
+data_knn['tiempo'] = data_knn['tiempo'] / 1000000000
+data_psa['tiempo'] = data_psa['tiempo'] / 1000000000
+datas_knn = data_knn.groupby('cantImagenes')
+datas_psa = data_psa.groupby('cantImagenes')
 
 plt.clf()
 
-plot_grafo = (data_kaggle['score'].median()).plot(fontsize=13, figsize=(11,8), color=colores[0],
+plot_grafo = (datas_knn['tiempo'].median()).plot(fontsize=13, figsize=(11,8), color=colores[0],
                 linestyle='--', marker='o')
+(datas_psa['tiempo'].median()).plot(
+    ax=plot_grafo,
+    fontsize=13, figsize=(11,8), color=colores[1],
+                linestyle='--', marker='o'
+    )
 
-plot_grafo.set_title("Accuracy en Kaggle - PSA + kNN, k = 4, \n42000 imágenes de training", fontsize = 15)
-plot_grafo.set_ylabel("Accuracy", size = 14)
-plot_grafo.set_xlabel("Alpha", size = 14)
-plot_grafo.legend(["Accuracy PCA - 42000"], fontsize = 14)
 
-plt.ylim([0.9,1.04])
-plt.xlim([25,38])
+plot_grafo.set_title("Tiempos en segundos variando la cantidad de imágenes", fontsize = 15)
+plot_grafo.set_ylabel("Tiempo (segundos)", size = 14)
+plot_grafo.set_xlabel("Cantidad de imágenes", size = 14)
+plot_grafo.legend(["kNN", "PCA"], fontsize = 14)
+
+# plt.ylim([0.9,1.04])
+# plt.xlim([25,38])
 
 plt.show()
