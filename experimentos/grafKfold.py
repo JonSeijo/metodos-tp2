f030de9e7f84cfa253b7e06117823683e3ffe9a9
jonseijo <jon.seijo@gmail.com>
2017-11-24 23:47:24 -0300

    Nuevo formato de graficos para la reentrega

--- experimentos/grafKfold.py
+++ experimentos/grafKfold.py
@@ -101,20 +101,38 @@ eje_x = []
 
 clase_elegida = 9
 
+precision_por_clase = [[] for _ in range(10)]
+recall_por_clase = [[] for _ in range(10)]
+f1_por_clase = [[] for _ in range(10)]
+ejex_por_clase = [[] for _ in range(10)]
+
 for j in range(0, len(precision)):
-    if j%10 == clase_elegida: #cambiar acá para hacer un grafico para c/ clase
-        pres_0.append(precision[j])
-        recall_0.append(recall[j])
-        f1_0.append(f1[j])
-        eje_x.append(Kkfold[j])
+    # if j%10 == clase_elegida: #cambiar acá para hacer un grafico para c/ clase
 
-        tiempototalknn_0.append(tiempototalknn[j])
-        tiempototalfold_0.append(tiempototalfold[j])
+    clase_actual = j%10
+    precision_por_clase[clase_actual].append(precision[j])
+    recall_por_clase[clase_actual].append(recall[j])
+    f1_por_clase[clase_actual].append(f1[j])
+    ejex_por_clase[clase_actual].append(Kkfold[j])
+
+    # tiempototalknn_0.append(tiempototalknn[j])
+    # tiempototalfold_0.append(tiempototalfold[j])
 
 
-for j in range(0, len(tiempototalfold_0)):
-    tiempototalfold_0[j] /= 1000000000.0
-    tiempototalknn_0[j] /= 1000000000.0
+# for j in range(0, len(precision)):
+#     if j%10 == clase_elegida: #cambiar acá para hacer un grafico para c/ clase
+#         pres_0.append(precision[j])
+#         recall_0.append(recall[j])
+#         f1_0.append(f1[j])
+#         eje_x.append(Kkfold[j])
+
+#         tiempototalknn_0.append(tiempototalknn[j])
+#         tiempototalfold_0.append(tiempototalfold[j])
+
+
+# for j in range(0, len(tiempototalfold_0)):
+#     tiempototalfold_0[j] /= 1000000000.0
+#     tiempototalknn_0[j] /= 1000000000.0
 
 # print(precision)
 # print(pres_0)
@@ -135,19 +153,37 @@ for j in range(0, len(tiempototalfold_0)):
 # plt.plot(eje_x, recall_0, '-o', label="recall")
 # plt.plot(eje_x, f1_0, '-o',  label="f1")
 
-plt.plot(eje_x, pres_0, label="precision")
-plt.plot(eje_x, f1_0,  label="f1")
-plt.plot(eje_x, recall_0, label="recall")
+t_PRECISION = "Precision"
+t_RECALL = "Recall"
+t_F1 = "F1"
 
-#plt.ylabel('Tiempo (seg)')
-plt.ylabel('Puntaje')
-plt.xlabel('k fold')
-plt.title('Clase ' + str(clase_elegida), fontsize=14)
-plt.ylim([0.8,1.1])
+# TIPO = t_PRECISION
+# TIPO = t_RECALL
+TIPO = t_F1
 
-#plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+clases = [0, 1, 2, 5, 7, 9]
+plt.title(TIPO + ' por clase', fontsize=14)
+plt.ylabel(TIPO)
+plt.xlabel('K-fold')
 
-plt.legend(["Precision", "F1", "Recall"])
+for clase in clases:
+    if (TIPO == t_PRECISION):
+        plt.plot(ejex_por_clase[clase], precision_por_clase[clase])
+    elif (TIPO == t_RECALL):
+        plt.plot(ejex_por_clase[clase], recall_por_clase[clase])
+    elif (TIPO == t_F1):
+        plt.plot(ejex_por_clase[clase], f1_por_clase[clase])
+
+
+# plt.plot(eje_x, pres_0, label="precision")
+# plt.plot(eje_x, f1_0,  label="f1")
+# plt.plot(eje_x, recall_0, label="recall")
+
+# plt.ylim([0.85,0.98])
+
+# #plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+
+plt.legend(["Clase " + str(clase) for clase in clases])
 
 # plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
            # ncol=2, mode="expand", borderaxespad=0.)
